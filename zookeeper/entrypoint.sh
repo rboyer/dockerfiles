5bb00f584893a6aed1fab9f452dcfadca6e93f69
R.B. Boyer <public@richardboyer.net>
2016-07-20 09:29:16 -0500

    new zookeeper

--- zookeeper/entrypoint.sh
+++ zookeeper/entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/dumb-init /bin/sh
+set -e
 
-set -e -o pipefail
+# Note above that we run dumb-init as PID 1 in order to reap zombie processes
+# as well as forward signals to all processes in its session. Normally, sh
+# wouldn't do either of these functions so we'd leak zombies as well as do
+# unclean termination of all our sub-processes.
 
 echo "1" > /var/lib/zookeeper/myid
 
@@ -12,13 +16,10 @@ echo "log4j.appender.CONSOLE.Threshold=INFO" >> $log4j_file
 echo "log4j.appender.CONSOLE.layout=org.apache.log4j.PatternLayout" >> $log4j_file
 echo "log4j.appender.CONSOLE.layout.ConversionPattern=%d{ISO8601} - %-5p [%t:%C{1}@%L] - %m%n" >> $log4j_file
 
-
 . /etc/zookeeper/conf/environment
 
-#exec start-stop-daemon --start -c $USER --exec $JAVA --name zookeeper \
-#	-- -cp $CLASSPATH $JAVA_OPTS -Dzookeeper.log.dir=${ZOO_LOG_DIR} \
-#	-Dzookeeper.root.logger=${ZOO_LOG4J_PROP} $ZOOMAIN $ZOOCFG
+chown -R app:app /var/lib/zookeeper /var/log/zookeeper
 
-exec $JAVA -cp $CLASSPATH $JAVA_OPTS \
+exec gosu app $JAVA -cp $CLASSPATH $JAVA_OPTS \
 	-Dzookeeper.root.logger=${ZOO_LOG4J_PROP} \
 	$ZOOMAIN $ZOOCFG
