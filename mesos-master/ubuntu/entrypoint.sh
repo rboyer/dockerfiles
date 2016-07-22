7cfcc4683a460080722e11a98679134fb8664ae7
R.B. Boyer <public@richardboyer.net>
2016-07-22 10:20:26 -0500

    fix perms

--- mesos-master/ubuntu/entrypoint.sh
+++ mesos-master/ubuntu/entrypoint.sh
@@ -6,4 +6,6 @@ set -e
 # wouldn't do either of these functions so we'd leak zombies as well as do
 # unclean termination of all our sub-processes.
 
+chown -R mesos:mesos /var/lib/mesos
+
 exec gosu mesos mesos-master "$@"
