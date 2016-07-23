4fc851215320537deaec80c52846931229451c68
R.B. Boyer <public@richardboyer.net>
2016-07-23 12:51:41 -0500

    also zk

--- pullall.sh
+++ pullall.sh
@@ -5,13 +5,13 @@ set -e -u -o pipefail
 images=(
     rboyer/base:ubuntu
     rboyer/base-java:ubuntu
+    rboyer/zookeeper:ubuntu
     rboyer/mesos:ubuntu
     rboyer/mesos-master:ubuntu
     rboyer/mesos-slave:ubuntu
     rboyer/marathon:ubuntu
 )
 # rboyer/steam:latest
-# rboyer/zookeeper:ubuntu
 # rboyer/devmesos:latest
 
 for img in "${images[@]}"; do
