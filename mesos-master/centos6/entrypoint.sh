85ea4b172dd077335e17cdeb55fc1a5fefe76e15
R.B. Boyer <public@richardboyer.net>
2016-08-04 09:44:04 -0500

    adding more c6

---
+++ mesos-master/centos6/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/dumb-init /bin/sh
+set -e
+
+# Note above that we run dumb-init as PID 1 in order to reap zombie processes
+# as well as forward signals to all processes in its session. Normally, sh
+# wouldn't do either of these functions so we'd leak zombies as well as do
+# unclean termination of all our sub-processes.
+
+chown -R mesos:mesos /var/lib/mesos
+
+exec gosu mesos mesos-master "$@"
