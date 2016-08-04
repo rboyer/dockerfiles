e2e3956a3f525692e1e8c77789f7bab4e865496d
R.B. Boyer <public@richardboyer.net>
2016-08-04 10:01:34 -0500

    also update pull script to pull all flavors

--- pullall.sh
+++ pullall.sh
@@ -10,6 +10,22 @@ images=(
     rboyer/mesos-master:ubuntu
     rboyer/mesos-slave:ubuntu
     rboyer/marathon:ubuntu
+
+    rboyer/base:centos
+    rboyer/base-java:centos
+    rboyer/zookeeper:centos
+    rboyer/mesos:centos
+    rboyer/mesos-master:centos
+    rboyer/mesos-slave:centos
+    rboyer/marathon:centos
+
+    rboyer/base:centos6
+    rboyer/base-java:centos6
+    rboyer/zookeeper:centos6
+    rboyer/mesos:centos6
+    rboyer/mesos-master:centos6
+    rboyer/mesos-slave:centos6
+    rboyer/marathon:centos6
 )
 # rboyer/steam:latest
 # rboyer/devmesos:latest
