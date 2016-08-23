b23ca50273ca0720251db1eef3b30b8d94adeab4
R.B. Boyer <public@richardboyer.net>
2016-08-23 07:31:09 -0500

    experimental (incomplete) sadmesos

---
+++ _archive/sadmesos/centos6/apps/mesos-slave/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/dumb-init /bin/sh
+set -e
+
+# Note above that we run dumb-init as PID 1 in order to reap zombie processes
+# as well as forward signals to all processes in its session. Normally, sh
+# wouldn't do either of these functions so we'd leak zombies as well as do
+# unclean termination of all our sub-processes.
+
+exec mesos-slave "$@"
